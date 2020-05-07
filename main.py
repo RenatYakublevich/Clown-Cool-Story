83deb7a2f950eec722c283862ce096db32c023b5
Renat Yakublevich <49581209+RenatYakublevich@users.noreply.github.com>
2020-05-08 02:32:40 +0300

    version 1.1
    пофикшен баг c now и полный переход на бд основу

--- main.py
+++ main.py
@@ -4,18 +4,63 @@ import test_main
 import time
 import re
 import os
+import test2
+import mysql.connector
+from mysql.connector import Error
+
+
+def create_connection(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=db_name
+        )
+        print("Connection to MySQL DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+    return connection
+connection = create_connection("localhost", "mysql", "mysql","history_list")
+
+def execute_query(connection, query):
+     cursor = connection.cursor()
+     try:
+         cursor.execute(query)
+         connection.commit()
+         print("Query executed successfully")
+     except Error as e:
+         print(f"The error '{e}' occurred")
+
+def execute_read_query(connection, query):
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as e:
+        print("The error '{e}' occurred")
 
 bot = telebot.TeleBot('токен')
+
+
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=False)
     item1 = types.KeyboardButton('📖Прочитать истории')
     item2 = types.KeyboardButton('📚Добавить свою историю')
-
-    markup.add(item1,item2)
-    gg = 'Привет, <b>' + message.from_user.first_name.title() + '</b>!\nХочешь прочитать или добавить кринжовую историю про себя или своего друга? Тогда тебе к нам\nНу что,поехали?)😏\n\nСоздатель - Якублевич Ренат\nGithub - https://github.com/RenatYakublevich'.format(message.from_user, bot.get_me())
+    item3 = types.KeyboardButton('👨‍🔧О создателе')
+    markup.add(item1,item2,item3)
+    test2.create_userz(message.chat.id)
+    gg = 'Привет, <b>' + message.from_user.first_name.title() + '</b>!\nХочешь прочитать или добавить кринжовую историю про себя или своего друга? Тогда тебе к нам\nНу что,поехали?)😏'.format(message.from_user, bot.get_me())
     bot.send_message(message.chat.id,gg,parse_mode='html',reply_markup=markup)
+    bot.send_photo(message.chat.id, 'https://sun2.beltelecom-by-minsk.userapi.com/f0FkFdMP8XEOP54pAnoCSulnaqljjWJjoflD1w/48MQhmk4-5s.jpg')
+
+
 
 
 @bot.message_handler(content_types=['text'])
@@ -23,36 +68,46 @@ def crypto_price_telegram(message):
     if message.chat.type == 'private':
         if message.text == '📖Прочитать истории':
             markup = types.ReplyKeyboardMarkup(resize_keyboard=False)
-            item1 = types.KeyboardButton('Предыдущая история')
             item2 = types.KeyboardButton('Назад')
             item3 = types.KeyboardButton('Следующая история')
+            item4 = types.KeyboardButton('👍Поставить лайк')
 
-            markup.add(item1,item2,item3)
+            markup.add(item2,item3,item4)
 
-            bot.send_message(message.chat.id, test_main.text[test_main.now] ,reply_markup=markup)
+
+            bot.send_message(message.chat.id, test2.hax[test2.now_is(message.chat.id)] ,reply_markup=markup)
         if message.text == 'Следующая история':
-            if test_main.now == test_main.count:
-                test_main.now = 0
-                bot.send_message(message.chat.id, test_main.text[test_main.now])
-            else:
-                test_main.now +=1
-                bot.send_message(message.chat.id, test_main.text[test_main.now])
-        if message.text == 'Предыдущая история':
-            if test_main.now == -(test_main.count):
-                test_main.now = 0
-                bot.send_message(message.chat.id, test_main.text[test_main.now])
-            else:
-                test_main.now -=1
-                bot.send_message(message.chat.id, test_main.text[test_main.now])
-
+            if int(test2.now_is(message.chat.id)) == (test2.couunt() - 1):
+                test2.update_zero(message.chat.id)
+                bot.send_message(message.chat.id, test2.hax[test2.now_is(message.chat.id)])
+            else :
+                test2.update_now(message.chat.id)
+                print(test2.now_is(message.chat.id))
+                bot.send_message(message.chat.id, test2.hax[test2.now_is(message.chat.id)])
         if message.text == 'Назад':
             welcome(message)
+            test2.update_zero(message.chat.id)
         if message.text == '📚Добавить свою историю':
             bot.send_message(message.chat.id, 'Для добавления истории перейди на сайт и <b>поделись своей кул стори со всем миром аблсолютно анонимно!</b>\n\nhttps://cutt.ly/LylMio5',parse_mode='html')
 
+        if message.text == '👨‍🔧О создателе':
+            bot.send_message(message.chat.id, 'Создатель - Якублевич Ренат\nGithub - https://github.com/RenatYakublevich\nСотрудничество - https://vk.com/helloworldbastard')
+        if message.text == '👍Поставить лайк':
+            test2.go_like(message.chat.id,test2.now_is(message.chat.id) + 1)
 
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    try:
+        if call.message:
+            if call.data == 'like':
 
+                test2.go_like(message.chat.id,test2.now_is(message.chat.id))
+                # remove inline buttons
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Спасибо тебе за твою активность!😇",
+                reply_markup=None)
 
+    except Exception as e:
+        print(repr(e))
 
 
 
--- test2.py
+++ test2.py
@@ -49,15 +49,82 @@ def execute_read_query(connection, query):
     except Error as e:
         print(f"The error '{e}' occurred")
 
+def create_userz(telegram_id):
+    create_users = """
+        INSERT INTO
+          `users` (`telegram_id`)
+        VALUES
+          ('""" + str(telegram_id) + """');
+                    """
+    execute_query(connection, create_users)
+
+def couunt_users():
+    count = "SELECT COUNT(*) FROM `users`"
+    counts = execute_read_query(connection, count)
+    counts = str(counts)
+    counts = counts[2]
+    counts = int(counts)
+    return counts
+
+def count_is(telegram_id):
+    gg = """SELECT telegram_id = """ + str(telegram_id) + """
+            FROM history_likes
+            GROUP BY telegram_id
+            HAVING count(distinct id)=1"""
+    zxc = execute_read_query(connection, gg)
+    zxc = str(zxc)
+    return zxc
+
+def count_is_HH(telegram_id):
+    gg = """SELECT telegram_id = """ + str(telegram_id) + """
+            FROM users
+            GROUP BY telegram_id
+            HAVING count(distinct id)=1"""
+    zxc = execute_read_query(connection, gg)
+    zxc = str(zxc)
+    return zxc
+
+def now_is(telegram_id):
+    select_users = "SELECT * FROM `users` WHERE `telegram_id` = '" + str(telegram_id) + "'"
+    users = str(execute_read_query(connection, select_users))
+    users = users.split(',')  #2 это now
+    users = users[2]
+    users = users[1]
+    users = int(users)
+    return users
 
 
+def update_now(telegram):
+        update_post_description = "UPDATE `users` SET now = now + 1 WHERE telegram_id = " + str(telegram)
 
-select_users = "SELECT `history_text` from `history_list`"
-users = execute_read_query(connection, select_users)
+        execute_query(connection,  update_post_description)
 
-count = "SELECT COUNT(*) FROM `history_list`"
-counts = execute_read_query(connection, count)
-counts = str(counts)
-counts = counts[2]
-counts = int(counts)
-now = 0
+def update_zero(telegram):
+    update_post_description = "UPDATE `users` SET now = 0 WHERE telegram_id = " + str(telegram)
+
+    execute_query(connection,  update_post_description)
+
+def go_like(telegram,id_post):
+        create_users = """
+            INSERT INTO
+              `history_likes` (`telegram_id`,`history_id`)
+            VALUES
+              (""" + str(telegram) + """,""" + str(id_post) +  """);
+                        """
+        execute_query(connection, create_users)
+
+def like_is():
+    gg = "SELECT* FROM `history_likes` HAVING COUNT(*) = 1"
+    zxc = execute_read_query(connection, gg)
+    zxc = str(zxc)
+    return zxc
+
+ggq = "SELECT `history_text` FROM `history_list` "
+hax = execute_read_query(connection, ggq)
+def couunt():
+    count = "SELECT COUNT(*) FROM `history_list`"
+    counts = execute_read_query(connection, count)
+    counts = str(counts)
+    counts = counts[2]
+    counts = int(counts)
+    return counts
