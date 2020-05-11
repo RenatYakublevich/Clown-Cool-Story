ebbb4026aa35b34fa6e51082c23bb1a61d592bca
Renat Yakublevich <49581209+RenatYakublevich@users.noreply.github.com>
2020-05-11 14:56:47 +0300

    version 1.2
    добавлена функция сохранение историй и добавления историй прямо из бота

--- main.py
+++ main.py
@@ -4,49 +4,25 @@ import test_main
 import time
 import re
 import os
-import test2
 import mysql.connector
 from mysql.connector import Error
+from itertools import groupby
 
-
-def create_connection(host_name, user_name, user_password, db_name):
-    connection = None
-    try:
-        connection = mysql.connector.connect(
-            host=host_name,
-            user=user_name,
-            passwd=user_password,
-            database=db_name
-        )
-        print("Connection to MySQL DB successful")
-    except Error as e:
-        print(f"The error '{e}' occurred")
-
-    return connection
-connection = create_connection("localhost", "mysql", "mysql","history_list")
-
-def execute_query(connection, query):
-     cursor = connection.cursor()
-     try:
-         cursor.execute(query)
-         connection.commit()
-         print("Query executed successfully")
-     except Error as e:
-         print(f"The error '{e}' occurred")
-
-def execute_read_query(connection, query):
-    cursor = connection.cursor()
-    result = None
-    try:
-        cursor.execute(query)
-        result = cursor.fetchall()
-        return result
-    except Error as e:
-        print("The error '{e}' occurred")
+def countrovn():
+    count = open('history.txt')
+    count = count.read()
+    count1 = len(count)
+    xd = 0
+    count_final = 0
+    while count1 > xd:
+        if count[xd] == '=':
+            count_final += 1
+        xd +=1
+    return count_final \
 
 bot = telebot.TeleBot('токен')
 
-
+now = 0
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
@@ -54,8 +30,8 @@ def welcome(message):
     item1 = types.KeyboardButton('📖Прочитать истории')
     item2 = types.KeyboardButton('📚Добавить свою историю')
     item3 = types.KeyboardButton('👨‍🔧О создателе')
-    markup.add(item1,item2,item3)
-    test2.create_userz(message.chat.id)
+    item4 = types.KeyboardButton('⭐️Сохраненные истории')
+    markup.add(item1,item2,item3,item4)
     gg = 'Привет, <b>' + message.from_user.first_name.title() + '</b>!\nХочешь прочитать или добавить кринжовую историю про себя или своего друга? Тогда тебе к нам\nНу что,поехали?)😏'.format(message.from_user, bot.get_me())
     bot.send_message(message.chat.id,gg,parse_mode='html',reply_markup=markup)
     bot.send_photo(message.chat.id, 'https://sun2.beltelecom-by-minsk.userapi.com/f0FkFdMP8XEOP54pAnoCSulnaqljjWJjoflD1w/48MQhmk4-5s.jpg')
@@ -70,44 +46,75 @@ def crypto_price_telegram(message):
             markup = types.ReplyKeyboardMarkup(resize_keyboard=False)
             item2 = types.KeyboardButton('Назад')
             item3 = types.KeyboardButton('Следующая история')
-            item4 = types.KeyboardButton('👍Поставить лайк')
+            item4 = types.KeyboardButton('⭐️Сохранить историю')
 
             markup.add(item2,item3,item4)
 
+            story = open('history.txt')
+            story = story.read()
+            story = story.split('=')
+            bot.send_message(message.chat.id,story[0],reply_markup=markup)
 
-            bot.send_message(message.chat.id, test2.hax[test2.now_is(message.chat.id)] ,reply_markup=markup)
         if message.text == 'Следующая история':
-            if int(test2.now_is(message.chat.id)) == (test2.couunt() - 1):
-                test2.update_zero(message.chat.id)
-                bot.send_message(message.chat.id, test2.hax[test2.now_is(message.chat.id)])
+
+            global now
+            story = open('history.txt')
+            story = story.read()
+            story = story.split('=')
+            if now >= countrovn():
+                now = 0
+                bot.send_message(message.chat.id,story[now])
+                now +=1
             else :
-                test2.update_now(message.chat.id)
-                print(test2.now_is(message.chat.id))
-                bot.send_message(message.chat.id, test2.hax[test2.now_is(message.chat.id)])
+                now +=1
+                bot.send_message(message.chat.id,story[now])
         if message.text == 'Назад':
             welcome(message)
-            test2.update_zero(message.chat.id)
         if message.text == '📚Добавить свою историю':
-            bot.send_message(message.chat.id, 'Для добавления истории перейди на сайт и <b>поделись своей кул стори со всем миром аблсолютно анонимно!</b>\n\nhttps://cutt.ly/LylMio5',parse_mode='html')
+            msg = bot.send_message(message.chat.id, '✍️Отправь мне свою историю и я добавлю её в базу данных')
+            bot.register_next_step_handler(msg, process1)
 
         if message.text == '👨‍🔧О создателе':
             bot.send_message(message.chat.id, 'Создатель - Якублевич Ренат\nGithub - https://github.com/RenatYakublevich\nСотрудничество - https://vk.com/helloworldbastard')
-        if message.text == '👍Поставить лайк':
-            test2.go_like(message.chat.id,test2.now_is(message.chat.id) + 1)
 
-@bot.callback_query_handler(func=lambda call: True)
-def callback_inline(call):
+        if message.text == '⭐️Сохранить историю':
+            my_file = open("favourite.txt", 'a')
+            my_file.write('_' + str(now))
+            my_file.close()
+            listFav = open("favourite.txt", 'r')
+            skii = listFav.read()
+        if message.text == '⭐️Сохраненные истории':
+            story = open('history.txt')
+            story = story.read()
+            story = story.split('=')
+            saveStory = open('favourite.txt')
+            saveStory = saveStory.read()
+            count = saveStory.split('_')
+            def f(l):
+                n = []
+                for i in l:
+                    if i not in n:
+                        n.append(i)
+                return n
+            new_count = f(count)
+            trucount = 0
+
+            while len(new_count) > trucount:
+                bot.send_message(message.chat.id, story[int(new_count[trucount])])
+                trucount +=1
+
+
+def process1(message):
     try:
-        if call.message:
-            if call.data == 'like':
+        gg = message.text
 
-                test2.go_like(message.chat.id,test2.now_is(message.chat.id))
-                # remove inline buttons
-                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Спасибо тебе за твою активность!😇",
-                reply_markup=None)
+        f = open('history.txt', 'a')
+        f.write("\n" + '=' + gg)
+        f.close()
 
+        bot.reply_to(message,'📝Мы записали вашу тру стори в базу данных!')
     except Exception as e:
-        print(repr(e))
+        bot.reply_to(message, 'oooops')
 
 
 
